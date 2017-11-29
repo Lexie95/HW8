a95c96e6890d58da6887bc3afd7be1a7b25bb28b
Lexie95 <32326197+Lexie95@users.noreply.github.com>
2017-11-29 04:06:29 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var value = 0;
+
+function setup() {
+	createCanvas(windowWidth,windowHeight);
+    background(255)
+    noStroke();
+    fill('#FEB7E2');
+    ellipse(width/2, height/2,800,600);
+}
+
+function draw() {
+  fill(value);
+  ellipse(width/2-150, height/2,90);
+  ellipse(width/2+150, height/2,90);
+    
+  fill(0);
+  ellipse(width/2-150, height/2,10);
+  ellipse(width/2+150, height/2,10);
+
+}
+
+function deviceTurned() {
+  if (turnAxis == 'X'){
+    if (value == 0){
+      value = 255
+    } else if (value == 255) {
+      value = 0;
+    }
+  }
+}
+
+//50881
